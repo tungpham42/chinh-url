3a2d8f2841690dfa3853b25655c5abd61754c763
Tung Pham <tung.42@gmail.com>
2025-07-17 14:11:47 +0700

    brand logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Footer from "./components/Footer";
+import MainBrandLogo from "./components/MainBrandLogo";
 import RomanizeTool from "./components/RomanizeTool";
 
 const App = () => {
   return (
     <>
+      <MainBrandLogo
+        logoSrc="/soft-logo.webp"
+        mainDomain="soft.io.vn"
+        dismissible={false}
+        altText="Logo Soft"
+      />
       <RomanizeTool />
       <Footer />
     </>
